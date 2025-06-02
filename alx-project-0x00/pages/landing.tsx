a2e7b0c8842f4035483c4b9400727952edb1f29f
Abdelhamid_Marmar <abdelhamidmarmar4@gmail.com>
2025-06-02 21:02:01 +0100

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -5,7 +5,12 @@ const Landing: React.FC = () => {
   return (
     <div className="p-4">
       <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
-      <Card />
+      <Card
+        title="Modern Villa"
+        description="Spacious and luxurious."
+        price={320}
+        rating={4.9}
+      />
     </div>
   );
 };
